00896c8806650024d3d60cab0bc4bea41f933ba4
Brian Wu <57295844+tupleu@users.noreply.github.com>
2020-07-30 14:39:23 -0400

    Changed path

--- app/src/main/java/com/levigo/levigoapp/MainActivity.java
+++ app/src/main/java/com/levigo/levigoapp/MainActivity.java
@@ -118,9 +118,9 @@ public class MainActivity extends AppCompatActivity implements Serializable {
                             mNetworkName = document.get("network_name").toString();
                             mHospitalId = document.get("hospital_id").toString();
                             mHospitalName = document.get("hospital_name").toString();
-                            String inventoryRefUrl = "networks/network1/sites/n1_hospital3/n1_h3_departments/department1/n1_h1_d1 productids";
+//                            String inventoryRefUrl = "networks/network1/sites/n1_hospital3/n1_h3_departments/department1/n1_h1_d1 productids";
                             //TODO update to the following
-                            //String inventoryRefUrl = "networks/" + mNetworkId + "/hospitals/" + mHospitalId + "/departments/default_department/dis";
+                            String inventoryRefUrl = "networks/" + mNetworkId + "/hospitals/" + mHospitalId + "/departments/default_department/dis";
 
                             Toolbar mToolbar = findViewById(R.id.main_toolbar);
                             setSupportActionBar(mToolbar);
@@ -201,7 +201,7 @@ public class MainActivity extends AppCompatActivity implements Serializable {
                     switch (dc.getType()) {
                         case ADDED:
                             Log.d(TAG, "Added");
-                            dc.getDocument().getReference().collection("UDIs").addSnapshotListener(new EventListener<QuerySnapshot>() {
+                            dc.getDocument().getReference().collection("udis").addSnapshotListener(new EventListener<QuerySnapshot>() {
                                 @Override
                                 public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
                                     if (e != null) {
