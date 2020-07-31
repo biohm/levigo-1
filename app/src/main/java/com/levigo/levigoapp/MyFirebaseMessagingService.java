7287410af16addf50c86f86c6c2656eadc6f91ce
Riya Saha <rs3796@barnard.edu>
2020-07-31 01:18:49 -0400

    Create and Define FCM service

---
+++ app/src/main/java/com/levigo/levigoapp/MyFirebaseMessagingService.java
@@ -0,0 +1,22 @@
+package com.levigo.levigoapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+    private static final String TAG = MyFirebaseMessagingService.class.getSimpleName();
+
+    @Override
+    public void onNewToken(String token) {
+        Log.d("Token", "Refreshed token: " + token);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+    }
+}
